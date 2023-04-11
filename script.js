1421af126162212dd0a0886739551103e3301690
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-11 15:08:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return key in sampleObject;
+	// return key in sampleObject;
+	for(let i in sampleObject){
+    if(i===key){
+        return true
+    }
+    return false
+}
 }
 
 // Do not change the code below
